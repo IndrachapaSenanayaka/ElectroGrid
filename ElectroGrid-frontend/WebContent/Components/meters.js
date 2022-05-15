d267809b4285d321731b46ded60fd576f44317bf
Indrachapa Senanayaka <senanayakeindrachapa@gmail.com>
2022-05-15 20:17:23 +0530

    meter jQuery, Ajax component was created

--- ElectroGrid-frontend/WebContent/Components/meters.js
+++ ElectroGrid-frontend/WebContent/Components/meters.js
@@ -7,12 +7,121 @@ $(document).ready(function()
 	$("#alertError").hide();
 });
 
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event)
+{
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	// Form validation-------------------
+	var status = validateMeterForm();
+	if (status != true)
+	{
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	
+	// If valid------------------------
+	var type = ($("#hidMeterIDSave").val() == "") ? "POST" : "PUT";
+	$.ajax(
+	{
+		url : "MetersAPI",
+		type : type,
+		data : $("#formMeter").serialize(),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onMeterSaveComplete(response.responseText, status);
+		}
+	});
+});
+
+//onMeterSaveComplete
+function onMeterSaveComplete(response, status)
+{
+	if (status == "success")
+	{
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success")
+		{
+			$("#alertSuccess").text("Meter registered successfully.");
+			$("#alertSuccess").show();
+			$("#divMetersGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error")
+		{
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error")
+	{
+		$("#alertError").text("Error while registering the meter.");
+		$("#alertError").show();
+	} else
+	{
+		$("#alertError").text("Unknown error while registering the meter..");
+		$("#alertError").show();
+	}
+	$("#hidMeterIDSave").val("");
+	$("#formMeter")[0].reset();
+}
 
 
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function(event)
+{
+	$("#hidMeterIDSave").val($(this).data("meterid"));
+	$("#meterCode").val($(this).closest("tr").find('td:eq(0)').text());
+	$("#premisesID").val($(this).closest("tr").find('td:eq(1)').text());
+	$("#electricityAccountNo").val($(this).closest("tr").find('td:eq(2)').text());
+	$("#manufactureDate").val($(this).closest("tr").find('td:eq(3)').text());
+});
 
+//DELETE===========================================================
+$(document).on("click", ".btnRemove", function(event)
+{
+	$.ajax(
+	{
+		url : "MetersAPI",
+		type : "DELETE",
+		data : "meterID=" + $(this).data("meterid"),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onMeterDeleteComplete(response.responseText, status);
+		}
+	});
+});
 
-
-
+//onMeterDeleteComplete
+function onMeterDeleteComplete(response, status)
+{
+	if (status == "success")
+	{
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success")
+		{
+			$("#alertSuccess").text("Registered meter deleted successfully.");
+			$("#alertSuccess").show();
+			$("#divMetersGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error")
+		{
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error")
+	{
+		$("#alertError").text("Error while deleting the meter.");
+		$("#alertError").show();
+	} else
+	{
+		$("#alertError").text("Unknown error while deleting the meter..");
+		$("#alertError").show();
+	}
+}
 
 // CLIENT-MODEL================================================================
 function validateMeterForm()
