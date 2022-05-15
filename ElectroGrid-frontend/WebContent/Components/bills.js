aebc3d5c006a816b00eb6a719c9eabd4e2fd84e3
Indrachapa Senanayaka <senanayakeindrachapa@gmail.com>
2022-05-15 20:46:40 +0530

    bills jQuery, Ajax component was created

--- ElectroGrid-frontend/WebContent/Components/bills.js
+++ ElectroGrid-frontend/WebContent/Components/bills.js
@@ -7,6 +7,166 @@ $(document).ready(function()
 	$("#alertError").hide();
 });
 
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event)
+{
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	// Form validation-------------------
+	var status = validateBillForm();
+	if (status != true)
+	{
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	
+	// If valid------------------------
+	var type = ($("#hidBillIDSave").val() == "") ? "POST" : "PUT";
+	$.ajax(
+	{
+		url : "BillsAPI",
+		type : type,
+		data : $("#formBill").serialize(),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onBillSaveComplete(response.responseText, status);
+		}
+	});
+});
+
+//onBillSaveComplete
+function onBillSaveComplete(response, status)
+{
+	if (status == "success")
+	{
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success")
+		{
+			$("#alertSuccess").text("Electricity bill generated successfully.");
+			$("#alertSuccess").show();
+			$("#divBillsGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error")
+		{
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error")
+	{
+		$("#alertError").text("Error while generating the bill.");
+		$("#alertError").show();
+	} else
+	{
+		$("#alertError").text("Unknown error while generating the bill..");
+		$("#alertError").show();
+	}
+	$("#hidBillIDSave").val("");
+	$("#formBill")[0].reset();
+	$("#meterCode").removeAttr("disabled");
+}
+
+
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function(event)
+{
+	$("#hidBillIDSave").val($(this).data("billid"));
+	$("#meterCode").val($(this).data("metercode"));
+	$("#meterCode").attr("disabled", "disabled");
+	$("#monthStartedUnitsAmount").val($(this).data("startedunits"));
+	$("#monthEndedUnitsAmount").val($(this).data("endedunits"));
+});
+
+//DELETE===========================================================
+$(document).on("click", ".btnRemove", function(event)
+{
+	$.ajax(
+	{
+		url : "BillsAPI",
+		type : "DELETE",
+		data : "billID=" + $(this).data("billid"),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onBillDeleteComplete(response.responseText, status);
+		}
+	});
+});
+
+//onBillDeleteComplete
+function onBillDeleteComplete(response, status)
+{
+	if (status == "success")
+	{
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success")
+		{
+			$("#alertSuccess").text("Electricity bill Deleted successfully.");
+			$("#alertSuccess").show();
+			$("#divBillsGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error")
+		{
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error")
+	{
+		$("#alertError").text("Error while deleting the bill.");
+		$("#alertError").show();
+	} else
+	{
+		$("#alertError").text("Unknown error while deleting the bill..");
+		$("#alertError").show();
+	}
+}
+
+
+
+//VIEW===========================================================
+$(document).on("click", ".btnView", function(event)
+{
+	$.ajax(
+	{
+		url : "BillAPI",
+		type : "POST",
+		data : {"billID" : $(this).data("billid"), "accountNo" : $(this).data("accountno")},
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onBillViewComplete(response.responseText, status);
+		}
+	});
+});
+
+//onBillViewComplete
+function onBillViewComplete(response, status)
+{
+	if (status == "success")
+	{
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success")
+		{	
+			$("#divBillGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error")
+		{
+			$("#alertError").text("resultSet.data");
+			$("#alertError").show();
+		}
+	} else if (status == "error")
+	{
+		$("#alertError").text("Error while viewing the bill.");
+		$("#alertError").show();
+	} else
+	{
+		$("#alertError").text("Unknown error while viewing the bill..");
+		$("#alertError").show();
+	}
+}
+
 
 // CLIENT-MODEL================================================================
 function validateBillForm()
