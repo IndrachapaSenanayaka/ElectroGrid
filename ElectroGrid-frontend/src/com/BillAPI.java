5ee066a2ca4c40694d91df267f53acb9f384eef3
Indrachapa Senanayaka <senanayakeindrachapa@gmail.com>
2022-05-15 20:55:58 +0530

    One Bill API post method was created

---
+++ ElectroGrid-frontend/src/com/BillAPI.java
@@ -0,0 +1,92 @@
+package com;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BillAPI
+ */
+@WebServlet("/BillAPI")
+public class BillAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BillAPI() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map paras = getParasMap(request);
+
+		//create Meter object 
+		Consumer consumerObj = new Consumer();
+		
+		//caling the getConsumer method
+		HashMap<String, String> consumer = consumerObj.getConsumer(paras.get("accountNo").toString());
+		
+		//get firstName
+		String firstName = consumer.get("firstName");
+		
+		//get city
+		String city = consumer.get("city");
+		
+		//create ElectricityBill object 
+		ElectricityBill billObj = new ElectricityBill();
+		
+		//caling the viewBill method
+		String output = billObj.viewBill(paras.get("billID").toString(),
+				city,
+				firstName);
+		
+		response.getWriter().write(output);
+	}
+	
+	
+	// Convert request parameters to a Map
+		private static Map getParasMap(HttpServletRequest request)
+		{
+			Map<String, String> map = new HashMap<String, String>();
+			try
+			{
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+				String queryString = scanner.hasNext() ?
+									scanner.useDelimiter("\\A").next() : "";
+				scanner.close();
+				
+				String[] params = queryString.split("&");
+				for (String param : params)
+				{
+					String[] p = param.split("=");
+					map.put(p[0], p[1]);
+				}
+			}
+			catch (Exception e)
+			{
+			}
+			return map;
+		}	
+
+}
